829bda21f69e1b0603a3488c7e7716af0c2dfb09
harshardhan <harshavardhan.harish@gmail.com>
2025-05-06 11:45:29 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/MyBizApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBizApplication.class, args);
+	}
+
+}
